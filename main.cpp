840e5495cb019d0da6a1e7a313be1e09303af7c2
shazzad5709 <62200344+shazzad5709@users.noreply.github.com>
2022-02-16 16:46:13 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<stack>
+
+using namespace std;
+
+struct node
+{
+    string data;
+    struct node *next;
+};
+
+void addInStart (string text)
+{
+
+}
+
+void addAfter (int linenumber, string text)
+{
+
+}
+
+void addInBetween (int linenumber, string text)
+{
+    
+}
+
+int main()
+{
+    struct node *head;
+    struct node *tail;
+    int totallines=0;
+    int next = 1;
+    ofstream outfile;
+
+    int choice =- 1;
+    head = tail = NULL;
+
+
+    while(choice != 0)
+    {
+        cout<<"Please choose what you want to do: "<<endl;
+        cout<<"1. Insert text in Line N\n";
+        cout<<"2. Delete line N\n";
+        cout<<"3. Move line N into line M\n";
+        cout<<"4. Replace text in line N\n";
+        cout<<"5. Print all\n";
+        cout<<"6. Save into existing .txt file\n";
+        cout<<"7. Undo\n";
+        cout<<"8. Create a new .txt file\n";
+        cout<<"9. Print the next page\n";
+        cout<<"10. Print the previous page\n";
+        cin>>choice;
+        if(choice == 1)
+        {
+            int linenumber;
+            string text;
+            cout<<"Line number where the text will be placed into: ";
+            cin>>linenumber;
+            cout<<"Enter text: ";
+            cin.clear();
+            cin.sync();
+            getline(cin, text);
+            text+="\n";
+            if(linenumber == 1)
+                addInStart(text);
+            else if(linenumber > totallines)
+                addAfter(linenumber, text);
+            else
+                addInBetween(linenumber, text);
+        }
+
+    }
+    return 0;
+}
