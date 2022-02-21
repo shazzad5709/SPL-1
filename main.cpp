42d78727a9ca3c9468e5fd09d08ffa8c52d0ed3b
shazzad5709 <62200344+shazzad5709@users.noreply.github.com>
2022-02-21 17:12:43 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,80 +1,84 @@
 #include<iostream>
 #include<fstream>
-#include<string>
-#include<stack>
-
+#include<cstring>
+#include<map>
 using namespace std;
 
-struct node
-{
-    string data;
-    struct node *next;
-};
+map<string, bool> keywords;
+map<string, bool> varDec;
 
-void addInStart (string text)
+bool declaredVar(string s)
 {
-
+  
+  if(!varDec[s])
+    return varDec[s]=true;
+  else
+    return false;
 }
 
-void addAfter (int linenumber, string text)
+int isKeywords(string s)
 {
+	keywords["for"]=true;
+	keywords["if"]=true;
+  keywords["else if"]=true;
+  keywords["else"]=true;
+  keywords["while"]=true;
+  keywords["do"]=true;
+  keywords["break"]=true;
+  keywords["continue"]=true;
+  // keywords["if"]=2;
+  // keywords["if"]=2;
+}
+  
 
+
+bool substring(string line)
+{
+    // string s="print";
+    // for(int i=0; i<5; i++)
+    //     if(s[i]!=line[i])
+    //         return 0;
+    return 1;
 }
 
-void addInBetween (int linenumber, string text)
+void parse()
 {
-    
+  string line;
+  ifstream f;
+  int i=7;
+  f.open("huh.txt");
+  if (f.is_open())
+  {
+    while (getline(f, line) )
+    {
+      if(substring(line))
+      {
+        cout<<"    cout << \"";
+        while(line[i]!='"')
+          cout<<line[i++];
+        cout<<"\";";
+      }
+    }
+    f.close();
+  }
 }
+
 
 int main()
 {
-    struct node *head;
-    struct node *tail;
-    int totallines=0;
-    int next = 1;
-    ofstream outfile;
-
-    int choice =- 1;
-    head = tail = NULL;
-
-
-    while(choice != 0)
-    {
-        cout<<"Please choose what you want to do: "<<endl;
-        cout<<"1. Insert text in Line N\n";
-        cout<<"2. Delete line N\n";
-        cout<<"3. Move line N into line M\n";
-        cout<<"4. Replace text in line N\n";
-        cout<<"5. Print all\n";
-        cout<<"6. Save into existing .txt file\n";
-        cout<<"7. Undo\n";
-        cout<<"8. Create a new .txt file\n";
-        cout<<"9. Print the next page\n";
-        cout<<"10. Print the previous page\n";
-        cin>>choice;
-        if(choice == 1)
-        {
-            int linenumber;
-            string text;
-            cout<<"Line number where the text will be placed into: ";
-            cin>>linenumber;
-            cout<<"Enter text: ";
-            cin.clear();
-            cin.sync();
-            getline(cin, text);
-            text+="\n";
-            if(linenumber == 1)
-                addInStart(text);
-            else if(linenumber > totallines)
-                addAfter(linenumber, text);
-            else
-                addInBetween(linenumber, text);
-        }
-        if(choice == 2)
-        {
-            
-        }
-
-    }
-    return 0;
+    cout<<"Algorithm to C++"<<endl;
+    cout<<"-----------------"<<endl;
+    string line;
+    char c;
+    ofstream f;
+    f.open("huh.txt");
+    getline(cin, line);
+    f<<line;
+    f.close();
+    cout <<"---------------------------------------------------" << endl;
+    cout<<"#include<iostream>"<<endl;
+    cout<<"using namespace std;"<<endl;
+    cout<<"int main()"<<endl<<"{"<<endl;
+    parse();
+    cout<<"\n    return 0;\n}";
 }
