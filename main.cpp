5b22d53a76cac42294be6210eb442c551ebbae4b
shazzad5709 <shazzad1111@gmail.com>
2022-03-23 23:30:30 +0600

    abc

--- main.cpp
+++ main.cpp
@@ -1,1 +1,30 @@
 #include<iostream>
+#include<fstream>
+#include<vector>
+
+using namespace std;
+
+vector<string> classNames;
+
+void classInput()
+{
+    cout<<"Name of the class: ";
+    
+    string tempName;
+    cin>>tempName;
+    classNames.push_back(tempName);
+
+}
+
+int main()
+{
+    int choice=1;
+    while(choice!=3)
+    {
+        classInput();
+        cout<<"1. Create new class\n";
+        cout<<"2. Create new interface\n";
+        cout<<"3. Terminate\n";
+        cin>>choice;
+    }
+}
