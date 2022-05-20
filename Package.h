552bcf8778b9be95055594a2fa6b4abb8113aa48
shazzad5709 <shazzad1111@gmail.com>
2022-05-20 15:10:14 +0600

    a

--- Package.h
+++ Package.h
@@ -1,33 +1,35 @@
 #pragma once
 
 #include <vector>
-#include "Rect.h"
+#include "Rectangle.h"
 #include "ClassData.h"
 
 #include "tinyxml2.h"
 
+using namespace std;
+
 struct Package
 {
-    std::string name;
-    Rect coords;
-    std::vector<Package> subPackages;
+    string name;
+    Rectangle coords;
+    vector<Package> subPackages;
 
     bool insert(const Package& sub)
     {
         if(!coords.contains(sub.coords))
             return false;
 
-        for(auto& subsub : subPackages)
-            if(subsub.insert(sub))
+        for(auto& subPac : subPackages)
+            if(subPac.insert(sub))
                 return true;
 
         subPackages.push_back(sub);
         return true;
     }
 
-    void makeDirectories(const std::string& dest)
+    void makeDirectories(const string& dest)
     {
-        std::string packageDest = dest + name + '/';
+        string packageDest = dest + name + '/';
 
         mkdir(packageDest.c_str());
 
@@ -35,10 +37,10 @@ struct Package
             sub.makeDirectories(packageDest);
     }
 
-    std::string getDestination(const ClassData& classData) const
+    string getDestination(const ClassData& classData) const
     {
         if(!coords.contains(classData.coords))
-            throw std::runtime_error("cannot parse destination!");
+            throw runtime_error("cannot parse destination!");
 
         for(auto& sub : subPackages)
             if(sub.coords.contains(classData.coords))
@@ -56,14 +58,14 @@ struct Package
         return 0;
     }
 
-    std::string getPath(const ClassData& from, const ClassData& to) const
+    string getPath(const ClassData& from, const ClassData& to) const
     {
         for(auto& sub : subPackages)
             if( sub.coords.contains(from.coords)
             &&  sub.coords.contains(to.coords) )
                 return sub.getPath(from, to);
 
-        std::string back;
+        string back;
 
         for(size_t k=0 ; k<getDepth(from); k++)
             back += "../";
