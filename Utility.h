70368f1b66597ead8bb0d386af65e0d3b078b512
shazzad5709 <62200344+shazzad5709@users.noreply.github.com>
2022-05-29 23:32:27 +0600

    Update Utility.h

--- Utility.h
+++ Utility.h
@@ -16,6 +16,57 @@ string normalizeName(const string& name)
     return normal;
 }
 
+void computePiTable(char *pat, int M, int *lps);
+
+int find(char *pat, char *txt) //uses KMP Pattern Matching algorithm
+{
+    int M = strlen(pat);
+    int N = strlen(txt);
+    int lps[M];
+    computePiTable(pat, M, lps);
+    int i = 0, j = 0;
+    while (i < N)
+    {
+        if (pat[j] == txt[i])
+            j++, i++;
+
+        if (j == M)
+            return i - j;
+
+        else if (i < N && pat[j] != txt[i])
+        {
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i = i + 1;
+        }
+    }
+}
+
+void computePiTable(char *pat, int M, int *lps)
+{
+    int len = 0;
+    lps[0] = 0;
+    int i = 1;
+    while (i < M)
+    {
+        if (pat[i] == pat[len])
+        {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else
+        {
+            if (len != 0)
+                len = lps[len - 1];
+            else
+                lps[i++] = 0;
+            
+        }
+    }
+}
+
 string capitalize(std::string str)
 {
     str[0]=toupper(str[0]);
